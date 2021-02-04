8b6f3dd1e268795bd089e1c6a1eb21db9ca8f1d5
denialfaro <denilorenaalfaro@hotmail.com>
2021-02-03 19:01:15 -0700

    correccion

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,12 +7,19 @@ public class Player : MonoBehaviour
     [SerializeField]
     float moveSpeed;
 
+    Animator anim;
+    
+    void Awake() {
+        anim = GetComponent<Animator>();
+    }
+
     void Update() {
         transform.Translate(Axis.normalized.magnitude * Vector3.forward * moveSpeed * Time.deltaTime);
         if (Axis != Vector3.zero)
         {
             transform.rotation = Quaternion.LookRotation(Axis);
         }
+        anim.SetFloat("move", Mathf.Abs(Axis.normalized.magnitude));
     }
     Vector3 Axis {
         get => new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
